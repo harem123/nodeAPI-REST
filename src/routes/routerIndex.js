a2b4a9cf2144c4e4dd2b5b06012c327be77da43d
harem123 <31325055+harem123@users.noreply.github.com>
2022-09-30 17:22:10 -0500

    fix endpoints organizations

--- src/routes/routerIndex.js
+++ src/routes/routerIndex.js
@@ -9,6 +9,8 @@ const v1genreCreator = require('../controllers/genreCreator')
 const v1RegistControl = require('../controllers/userControl')
 const v1ControlEmail = require('../controllers/nodemailer.js')
 
+// movie endpoints
+
 router.delete("/movie/deleteByTitle", v1ControlMovies.deleteByTitle);
 
 router.get("/movie/detail", v1ControlMovies.simpleDetails);
@@ -31,25 +33,26 @@ router.delete("/character/deleteByname", v1ControlCharacters.deleteByName);
 
 router.get("/character/detail", v1ControlCharacters.charDetails);
 
+// register and login endpoints
+
+router.post("/auth/register", v1RegistControl.registerUser);
+
+router.post("/auth/login", v1RegistControl.login);
+
+// genre creation endpoint 
+
+router.post("/createGenre", v1genreCreator.createGenre);
+
 // other endpoints
 
 router.post("/upload", v1ControlImages.upload);
-
-router.post("/createGenre", v1genreCreator.createGenre);
 
 router.get("/files/:name", v1ControlImages.download);
 
 router.get("/email" , v1ControlEmail.sendEmail);
 
-router.post("/auth/register", v1RegistControl.registerUser);
-
 router.post("/protected", v1RegistControl.ensureToken,v1RegistControl.protectedSection);
 
-router.post("/auth/login", v1RegistControl.login);
-
-//router.patch("/:patchId", v1Controller.updateOneWorkout);
-
-//router.delete("/:deleteById", v1Controller.deleteOneWorkout);  
 
 // exporting modules
 module.exports = router
